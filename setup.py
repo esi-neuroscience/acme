c7d74fbe2f56d4558de8216a9e514e01fb35fca7
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2020-11-12 18:14:03 +0100

    NEW: DevOps Enhancements
    - put together GitHub <-> GitLab syncing script for ACME repositories
    - put together MANIFEST file for pip-deployment
    - put together setup.py + setup.cfg for package building
    - included automatic git versioning system and dynamic version fetching
      for test-PyPI deployment
    - modified conda2pip.py to consolidate conda/pip dependency management
    
    On branch dev
    Changes to be committed:
    	new file:   MANIFEST.in
    	modified:   acme.yml
    	modified:   acme/__init__.py
    	new file:   conda2pip.py
    	new file:   setup.cfg
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# Builtin/3rd party package imports
+import datetime
+import ruamel.yaml
+from setuptools import setup
+from setuptools_scm import get_version
+
+# Local imports
+from conda2pip import conda2pip
+
+# Get necessary and optional package dependencies
+required, dev = conda2pip(return_lists=True)
+
+# FIXME: uncomment once citation is needed
+# # Get package version for citationFile (for dev-builds this might differ from
+# # test-PyPI versions, which are ordered by recency)
+# version = get_version(root='.', relative_to=__file__)
+
+# # Update citation file
+# citationFile = "CITATION.cff"
+# yaml = ruamel.yaml.YAML()
+# with open(citationFile) as fl:
+#     ymlObj = yaml.load(fl)
+# ymlObj["version"] = version
+# ymlObj["date-released"] = datetime.datetime.now().strftime("%Y-%m-%d")
+# with open(citationFile, "w") as fl:
+#     yaml.dump(ymlObj, fl)
+
+# Run setup (note: identical arguments supplied in setup.cfg will take precedence)
+setup(
+    # install_requires=required,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    # extras_require={"dev": dev}
+)
