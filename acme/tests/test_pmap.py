d68710392225bdd5588a76cecb797b7fc60df224
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2020-11-30 21:26:53 +0100

    NEW: First (incomplete) draft of pytest class
    - started putting together testing code based on submitted
      GitHub issues
    
    On branch dev
    Changes to be committed:
    	modified:   acme/tests/test_pmap.py

--- acme/tests/test_pmap.py
+++ acme/tests/test_pmap.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 #
-# Simple script for testing acme w/o pip-installing it
+# Testing module for ACME's `ParallelMap` interface
 #
 
 # Builtin/3rd party package imports
-import numpy as np
-
-# Add acme to Python search path
 import os
-import sys
-acme_path = os.path.abspath(".." + os.sep + "..")
-if acme_path not in sys.path:
-    sys.path.insert(0, acme_path)
+import h5py
+import shutil
+import pytest
+import numpy as np
+from scipy import signal
 
 # Import main actor here
 from acme import ParallelMap
 
+# Functions that act as stand-ins for user-funcs
 def simple_func(x, y, z=3):
     return (x + y) * z
 
@@ -25,12 +24,7 @@ def medium_func(x, y, z=3, w=np.ones((3, 3))):
 def hard_func(x, y, z=3, w=np.zeros((3, 1)), **kwargs):
     return sum(x) + y,  z * w
 
-# Imports for tests
-from scipy import signal
-import h5py
-import shutil
-
-def lowpass_h5dset(arr_like, b, a, channel_no, padlen=200):
+def lowpass_hard(arr_like, b, a, channel_no, padlen=200):
     channel = arr_like[:, channel_no]
     res = signal.filtfilt(b, a, channel, padlen=padlen)
     return res
@@ -43,8 +37,9 @@ def lowpass_simple(h5name, channel_no):
     res = signal.filtfilt(b, a, channel, padlen=200)
     return res
 
-# Prepare code to be executed using, e.g., iPython's `%run` magic command
-if __name__ == "__main__":
+
+# Main testing class
+class TestParallelMap():
 
     # Construct linear combination of low- and high-frequency sine waves
     # and use an IIR filter to reconstruct the low-frequency component
@@ -59,60 +54,122 @@ if __name__ == "__main__":
     cutoff = 50
     b, a = signal.butter(8, 2 * cutoff / fs)
 
-    # Blow up the signal to have "channels" and "trials" and inflate the low-
-    # frequency component accordingly for ad-hoc comparisons later
+    # Blow up the signal to have "channels" and "trials": even/odd channels have
+    # opposing periodicity; do the same to the low-freq component
     sig = np.repeat(sig.reshape(-1, 1), axis=1, repeats=nChannels)
+    sig[:, ::2] *= -1
     sig = np.tile(sig, (nTrials, 1))
     orig = np.repeat(orig.reshape(-1, 1), axis=1, repeats=nChannels)
+    orig[:, ::2] *= -1
     orig = np.tile(orig, (nTrials, 1))
 
-    tempDir = os.path.join(os.path.abspath(os.path.expanduser("~")), "acme_tmp")
-    os.makedirs(tempDir, exist_ok=True)
+    # Error tolerance for low-pass filtered results
+    tol = 1e-3
 
-    sigName = os.path.join(tempDir, "sigdata.h5")
-    origName = os.path.join(tempDir, "origdata.h5")
+    # Test setup of `ParallelMap` w/different functions args/kwargs
+    def test_init(self):
 
-    with h5py.File(sigName, "w") as sigFile:
-        dset = sigFile.create_dataset("data", data=sig)
-        dset.attrs["b"] = b
-        dset.attrs["a"] = a
+        # Basic functionality w/simplest conceivable user-func
+        pmap = ParallelMap(simple_func, [2, 4, 6, 8], 4)
+        pmap = ParallelMap(simple_func, [2, 4, 6, 8], y=4)  # pos arg referenced via kwarg, cfg #2
+        pmap = ParallelMap(simple_func, 0, 4, z=[3, 4, 5, 6])
+        pmap = ParallelMap(simple_func, [2, 4, 6, 8], [2, 2], n_inputs=2)
 
-    with h5py.File(origName, "w") as origFile:
-        origFile.create_dataset("data", data=sig)
+        # User func has `np.ndarray` as keyword
+        pmap = ParallelMap(medium_func, [2, 4, 6, 8], y=[2, 2], n_inputs=2)
+        pmap = ParallelMap(medium_func, None, None, w=[np.ones((3, 3)), 2 * np.ones((3,3))])
+        pmap = ParallelMap(medium_func, None, None, z=np.zeros((3,)))
+        pmap = ParallelMap(medium_func, None, None, z=np.zeros((3, 1)))
 
-    with ParallelMap(lowpass_simple, sigName, range(nChannels), logfile=True) as pmap:
-        pmap.compute()
+        # Lots of ways for this to go wrong...
+        pmap = ParallelMap(hard_func, [2, 4, 6, 8], 2, w=np.ones((3,)))
+        pmap = ParallelMap(hard_func, [2, 4, 6, 8], y=22, w=np.ones((7, 1)))
+        pmap = ParallelMap(hard_func, np.ones((3,)), 1, w=np.ones((7, 1)))
+        pmap = ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], z=np.array([1, 2]), w=np.ones((8, 1)), n_inputs=2)
+        pmap = ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], w=np.ones((8, 1)), n_inputs=4)
 
-    sigData = h5py.File(sigName, "r")["data"]
-    with ParallelMap(lowpass_h5dset, sigData, b, a, range(nChannels), n_inputs=nChannels, logfile=True) as pmap:
-        pmap.compute()
+        # Ensure erroneous/ambiguous setups trigger the appropriate errors:
+        # not enough positional args
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(simple_func, 4)
+            assert "simple_func expects 2 positional arguments ('x', 'y'), found 1" in str(valerr.value)
+        # invalid kwargs
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(simple_func, 4, 4, z=3, w=4)
+            assert "simple_func accepts at maximum 1 keyword arguments ('z'), found 2" in str(valerr.value)
+        # ill-posed parallelization: two candidate lists for input distribution
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(simple_func, [2, 4, 6, 8], [2, 2])
+            assert "automatic input distribution failed: found 2 objects containing 2 to 4 elements" in str(valerr.value)
+        # ill-posed parallelization: two candidate lists for input distribution (`x` and `w`)
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(medium_func, [1, 2, 3], None, w=[np.ones((3,3)), 2 * np.ones((3,3))])
+            assert "automatic input distribution failed: found 2 objects containing 2 to 3 elements." in str(valerr.value)
+        # invalid input spec
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(simple_func, [2, 4, 6, 8], [2, 2], n_inputs=3)
+            assert "No object has required length of 3 matching `n_inputs`" in str(valerr.value)
+        # invalid input spec: `w` expects a NumPy array, thus it is not considered for input distribution
+        with pytest.raises(ValueError) as valerr:
+            ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], w=np.ones((8, 1)), n_inputs=8)
+            assert "No object has required length of 8 matching `n_inputs`" in str(valerr.value)
 
-    # Close any open HDF5 files to not trigger any `WinError`s and clean up the tmp dir
-    sigData.file.close()
-    shutil.rmtree(tempDir, ignore_errors=True)
+    # Functionality tests: perform channel-concurrent low-pass filtering
+    def test_filter_example(self):
 
-    # Test stuff within here...
-    pmap = ParallelMap(simple_func, [2, 4, 6, 8], 4)
-    pmap = ParallelMap(simple_func, [2, 4, 6, 8], y=4)
-    pmap = ParallelMap(simple_func, 0, 4, z=[3, 4, 5, 6])
-    pmap = ParallelMap(simple_func, [2, 4, 6, 8], [2, 2], n_inputs=2)
+        # Create tmp directory and create data-containers
+        tempDir = os.path.join(os.path.abspath(os.path.expanduser("~")), "acme_tmp")
+        os.makedirs(tempDir, exist_ok=True)
+        sigName = os.path.join(tempDir, "sigdata.h5")
+        origName = os.path.join(tempDir, "origdata.h5")
+        with h5py.File(sigName, "w") as sigFile:
+            dset = sigFile.create_dataset("data", data=self.sig)
+            dset.attrs["b"] = self.b
+            dset.attrs["a"] = self.a
+        with h5py.File(origName, "w") as origFile:
+            origFile.create_dataset("data", data=self.orig)
 
-    pmap = ParallelMap(medium_func, [2, 4, 6, 8], [2, 2], n_inputs=2)
-    pmap = ParallelMap(medium_func, None, None, w=[np.ones((3, 3)), 2 * np.ones((3,3))])
-    pmap = ParallelMap(medium_func, None, None, z=np.zeros((3,)))
-    pmap = ParallelMap(medium_func, None, None, z=np.zeros((3, 1)))
+        # Parallelize across channels, write results to disk
+        with ParallelMap(lowpass_simple, sigName, range(self.nChannels)) as pmap:
+            resOnDisk = pmap.compute()
+        assert len(pmap.kwargv["outFile"]) == pmap.n_calls
+        resFiles = [os.path.join(pmap.kwargv["outDir"][0], outFile) for outFile in pmap.kwargv["outFile"]]
+        assert resOnDisk == resFiles
+        assert all(os.path.isfile(fle) for fle in resOnDisk)
 
-    pmap = ParallelMap(hard_func, [2, 4, 6, 8], 2, w=np.ones((3,)))
-    pmap = ParallelMap(hard_func, [2, 4, 6, 8], 2, w=np.ones((7, 1)))
-    pmap = ParallelMap(hard_func, np.ones((3,)), 1, w=np.ones((7, 1)))
-    pmap = ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], z=np.array([1, 2]), w=np.ones((8, 1)), n_inputs=2)
-    pmap = ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], w=np.ones((8, 1)), n_inputs=4)
+        # Compare compuated single-channel results to expected low-freq signal
+        for chNo, h5name in enumerate(resOnDisk):
+            with h5py.File(h5name, "r") as h5f:
+                assert np.mean(np.abs(h5f["result"][()] - self.orig[:, chNo])) < self.tol
 
-    # # Errors
-    # pmap = ParallelMap(simple_func, 4)
-    # pmap = ParallelMap(simple_func, 4, 4, z=3, w=4)
-    # pmap = ParallelMap(simple_func, [2, 4, 6, 8], [2, 2])
-    # pmap = ParallelMap(simple_func, [2, 4, 6, 8], [2, 2], n_inputs=3)
-    # pmap = ParallelMap(medium_func, [1, 2, 3], None, w=[np.ones((3,3)), 2 * np.ones((3,3))])
-    # pmap = ParallelMap(hard_func, [2, 4, 6, 8], [2, 2], w=np.ones((8, 1)), n_inputs=8)
+        # Same, but collect results in memory: ensure nothing freaky happens
+        with ParallelMap(lowpass_simple, sigName, range(self.nChannels), write_worker_results=False) as pmap:
+            resInMem = pmap.compute()
+        for chNo in range(self.nChannels):
+            assert np.mean(np.abs(resInMem[chNo][0] - self.orig[:, chNo])) < self.tol
 
+        # use taskID in userfunc and store results somewhere else
+        # test cleanup
+        # ensure logfile is written (in combo w/verbose)
+        # ensure SLURM options are respected (njobs, partition, mem_per_job)
+
+        # with ParallelMap(lowpass_simple, sigName, range(nChannels), logfile=True) as pmap:
+        #     pmap.compute()
+
+        # Close any open HDF5 files to not trigger any `OSError`s and clean up the tmp dir
+        # sigData.file.close()
+        shutil.rmtree(tempDir, ignore_errors=True)
+
+    # # test esi-cluster-setup called separately before pmap
+    # def test_existing_cluster(self, testcluster):
+    #     pass
+    #     # # repeat selected test w/parallel processing engine
+    #     # # ensure client stays alive
+    #     # client = dd.Client(testcluster)
+    #     # par_tests = ["test_relative_array_padding",
+    #     #              "test_absolute_nextpow2_array_padding",
+    #     #              "test_object_padding",
+    #     #              "test_dataselection"]
+    #     # for test in par_tests:
+    #     #     getattr(self, test)()
+    #     # client.close()
