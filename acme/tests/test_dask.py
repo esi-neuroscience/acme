287f5eb1162469234a234ab7ab31379aa536d3cd
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2022-11-04 16:42:22 +0100

    FIX: Updated dask-helper tests
    - restructured slurm_setup tests
    - repaired tests to not crash on local machines
    
     On branch dev
     Changes to be committed:
    	modified:   acme/tests/test_dask.py

--- acme/tests/test_dask.py
+++ acme/tests/test_dask.py
@@ -9,83 +9,48 @@ import getpass
 import numpy as np
 
 # Import main actors here
-from acme.dask_helpers import customIOError, customValueError, customTypeError
+from acme.dask_helpers import customIOError
 from acme import cluster_cleanup, esi_cluster_setup, slurm_cluster_setup
-from conftest import skip_if_not_linux, useSLURM, onESI, defaultQ
+from conftest import useSLURM, onESI, defaultQ
 
 def test_cluster_setup():
-
-    # ESI-cluster specific:
-    # - don't start new cluster on top of existing one
-    # - specify n_cores
-    # - specify job_extra
-    # - specify output
-    # - get cpu count from mem spec
 
     # Tests which should work on any SLURM cluster
     if useSLURM:
 
-        defaultInput = {"partition" : defaultQ,
-                        "n_cores" : 1,
-                        "n_workers": 1,
-                        "processes_per_worker" : 1,
-                        "mem_per_worker": "1GB",
-                        "n_workers_startup" : 1,
-                        "timeout" : 180,
-                        "interactive" : False,
-                        "interactive_wait" : 10,
-                        "start_client" : True,
-                        "job_extra" : []}
-
         # Ensure invalid partition/memory specifications are caught
         with pytest.raises(ValueError):
-            faultyInput = dict(defaultInput)
-            faultyInput["partition"] = "invalid"
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition="invalid")
         cluster_cleanup()
         with pytest.raises(TypeError):
-            faultyInput = dict(defaultInput)
-            faultyInput["mem_per_worker"] = (3)
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, mem_per_worker=(3))
         cluster_cleanup()
         with pytest.raises(ValueError):
-            faultyInput = dict(defaultInput)
-            faultyInput["mem_per_worker"] = "invalidGB"
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, mem_per_worker="invalidGB")
         cluster_cleanup()
         with pytest.raises(ValueError):
-            faultyInput = dict(defaultInput)
-            faultyInput["mem_per_worker"] = "-110MB"
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, mem_per_worker="-110MB")
         cluster_cleanup()
 
         # Ensure additional sbatch parameters are processed correctly
         with pytest.raises(TypeError):
-            faultyInput = dict(defaultInput)
-            faultyInput["job_extra"] = "--output"
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, job_extra="--output")
         cluster_cleanup()
         with pytest.raises(ValueError):
-            faultyInput = dict(defaultInput)
-            faultyInput["job_extra"] = ["invalid"]
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, job_extra=["invalid"])
         cluster_cleanup()
-        customInput = dict(defaultInput)
-        customInput["job_extra"] = ["--job-name='averycustomjobname'"]
-        client = slurm_cluster_setup(**customInput)
+        client = slurm_cluster_setup(partition=defaultQ,
+                                     job_extra=["--job-name='averycustomjobname'"])
         assert 'averycustomjobname' in client.cluster.job_header
         cluster_cleanup()
 
         # Ensure output directory specification is parsed for correctness
         with pytest.raises(ValueError):
-            faultyInput = dict(defaultInput)
-            faultyInput["job_extra"] = ["--output=/path/to/nowhere"]
-            slurm_cluster_setup(**faultyInput)
+            slurm_cluster_setup(partition=defaultQ, job_extra=["--output=/path/to/nowhere"])
         cluster_cleanup()
-        customInput = dict(defaultInput)
         slurmOut = "/tmp/{}".format(getpass.getuser())
-        customInput["job_extra"] = ["--output={}".format(slurmOut), "--job-name='averycustomjobname'"]
-        client = slurm_cluster_setup(**customInput)
+        client = slurm_cluster_setup(partition=defaultQ,
+                                     job_extra=["--output={}".format(slurmOut), "--job-name='averycustomjobname'"])
         assert "--output={}".format(slurmOut) in client.cluster.job_header
         assert 'averycustomjobname' in client.cluster.job_header
         cluster_cleanup(client)
@@ -111,24 +76,25 @@ def test_cluster_setup():
             assert "--cpus-per-task=1" in client.cluster.job_header
             cluster_cleanup(client)
 
+    else:
+
+        # Trigger an exception by invoking `slurm_cluster_setup`` on non-SLURM node
+        with pytest.raises(customIOError) as err:
+            slurm_cluster_setup()
+            assert "Cannot access SLURM queuing system" in str(err)
+
+
 def test_local_setup():
     pass
 
+
 def test_backcompat_cluster():
 
-    deprecatedInput = {"partition" : defaultQ,
-                       "n_cores" : 1,
-                       "n_jobs": 1,
-                       "workers_per_job" : 1,
-                       "mem_per_job": "1GB",
-                       "n_jobs_startup" : 1,
-                       "timeout" : 180,
-                       "interactive" : False,
-                       "interactive_wait" : 10,
-                       "start_client" : True,
-                       "job_extra" : []}
-
-    client = slurm_cluster_setup(**deprecatedInput)
+    client = slurm_cluster_setup(partition=defaultQ,
+                                 n_jobs=1,
+                                 workers_per_job=1,
+                                 mem_per_job="1GB",
+                                 n_jobs_startup=1)
     assert len(client.cluster.workers) == 1
     memory = [w["memory_limit"] for w in client.cluster.scheduler_info["workers"].values()]
     assert round(memory[0] / 1000**3) == 1
