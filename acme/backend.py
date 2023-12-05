fdf006b5c0d955b3ec4898dacc05f60d298328b7
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-12-05 12:13:47 +0100

    FIX: Rollback dask worker callback removal
    - still necessary: without explicitly forwarding the Python-path, workers
      "forget" the caller environment during successive invocations.
    
    On branch release_preps
    Changes to be committed:
    	modified:   acme/backend.py

--- acme/backend.py
+++ acme/backend.py
@@ -19,6 +19,7 @@ import os
 import sys
 import glob
 import shutil
+import functools
 import pickle
 import logging
 import multiprocessing
@@ -809,6 +810,13 @@ class ACMEdaemon(object):
             raise RuntimeError(msg%(self.objName, self.client))
         log.debug("Found %d workers in client %s",
                   count_online_workers(self.client.cluster), str(self.client))
+
+        # Dask does not correctly forward the `sys.path` from the parent process
+        # to its workers. Fix this.
+        def init_acme(dask_worker, syspath):
+            sys.path = list(syspath)
+        self.client.register_worker_callbacks(setup=functools.partial(init_acme, syspath=sys.path))
+        log.debug("Registered worker callback to forward `sys.path`")
 
         # Format positional arguments for worker-distribution: broadcast all
         # inputs that are used by all workers and create a list of references
